dcfe99927405880ec5a5bd2c1b27fe178f5dac6e
MrunaliDeore <mrunalideoreohk1@gmail.com>
2022-07-26 11:40:46 +0530

    Added : main class file added

---
+++ main.java
@@ -0,0 +1,11 @@
+package Fifteen;
+
+public class main {
+
+	public static void main(String[] args){
+		System.out.println("Day 15 Assignment\n");
+		System.out.println("Section 3: Hash Tables\n");
+		System.out.println("Section 4: Binary Search Tree (BST)");
+	}
+
+}
